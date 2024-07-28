a45e28a0934cd15457e000bf4f3f37733ad21919
i-am-the-robot <titilolasalisukazeem1@gmail.com>
2024-07-28 18:20:32 +0100

    Update Streamlit_code_git.py

--- Streamlit_code_git.py
+++ Streamlit_code_git.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import streamlit as st
 import pickle
-
-model_path = 'house-price/House_model1.pkl'
-model_in = open(model_path, 'rb')
-pred = pickle.load(model_in)
+pred = pickle.load(open('House_model','rb'))
+#model_path = 'house-price/House_model1.pkl'
+#model_in = open(model_path, 'rb')
+#pred = pickle.load(model_in)
 
 loc_dic = {
     "urban": 2,
