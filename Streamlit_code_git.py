9965dc255dd03b1945c0ed889f96ea057b6460df
i-am-the-robot <titilolasalisukazeem1@gmail.com>
2024-07-28 14:19:32 +0100

    Add files via upload

---
+++ Streamlit_code_git.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import streamlit as st
+import pickle
+
+
+
+
+
+model_in = open('C:\\Users\PC\House_model1.pkl', 'rb')
+pred = pickle.load(model_in)
+
+loc_dic = {
+    "urban": 2,
+    "suburban" : 1,
+    "rural"  : 0
+    }
+
+def predict_price(data):
+    predicted_price=pred.predict(data)[0]
+    return predicted_price
+
+    
+def main():
+    st.title("Welcome to the OOU Real Estate Platform")
+    st.header('Be Ready to Get The Best!')
+    global username, show_welcome_page
+    username= st.text_input("Your Name")
+    
+    st.write("Wecome, ", username)
+    
+    st.write("LET US KNOW YOUR TASTE")
+    global no_bedroom, no_bathroom, location, area, house_age
+    house_age = int(st.number_input("House Age"))
+    no_bedroom = st.number_input(" Number of Bedroom")
+    no_bathroom = st.number_input(" Number of Bathroom")
+    area = st.number_input ("Area in Square ft ")
+    location_word =st.text_input(" Location (Rural, SubUrban or Urban Area?)").lower()
+    location = loc_dic.get(location_word, 0)
+    
+    
+    if st.button("House Price"):
+        user_data=pd.DataFrame({
+        "House Age" : [house_age],
+        "Numbers of Bedrooms": [no_bedroom],
+        "Number of Bathrooms" :[no_bathroom],
+        "Square Feet" : [area], 
+        "Location" : [location]
+        })
+    
+        predicted_price = predict_price(user_data)
+        predicted_price_rounded = round(predicted_price, 2) #rounded to 2 decimal places
+        
+        
+        st.write("Base on your requirements, the House price is: ", predicted_price_rounded)
+     
+    
+    
+        
+    
+    
+
+
+
+if __name__ == "__main__":
+    main() 
